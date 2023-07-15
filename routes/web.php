09aeb48ff3b0f45c8fd9ea5887530eacd14a2736
nandacjo <nandacoje22@gamil.com>
2023-07-15 10:56:34 -0400

    fix migration customers yang hilang

--- routes/web.php
+++ routes/web.php
@@ -17,25 +17,25 @@ use App\Http\Controllers\DashboardController;
 */
 
 // Dashboard
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+// Route::middleware('')->group(function () {
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
-    // Customers
-    Route::get('/customers', [CustomerController::class, 'index'])->name('customers');
-    Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
-    Route::post('/customers', [CustomerController::class, 'store'])->name('customers');
-    Route::get('/customers/{id}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
-    Route::put('/customers/{id}/update', [CustomerController::class, 'update'])->name('customers.update');
-    Route::delete('/customers/{id}/destroy', [CustomerController::class, 'destroy'])->name('customers.destroy');
+// Customers
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers');
+Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
+Route::post('/customers', [CustomerController::class, 'store'])->name('customers');
+Route::get('/customers/{id}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+Route::put('/customers/{id}/update', [CustomerController::class, 'update'])->name('customers.update');
+Route::delete('/customers/{id}/destroy', [CustomerController::class, 'destroy'])->name('customers.destroy');
 
-    // Armadas
-    Route::get('/armadas', [ArmadaController::class, 'index'])->name('armadas.index');
-    Route::get('/armadas/create', [ArmadaController::class, 'create'])->name('armadas.create');
-    Route::post('/armadas/create', [ArmadaController::class, 'store'])->name('armadas');
-    Route::get('/armadas/{id}/edit', [ArmadaController::class, 'edit'])->name('armadas.edit');
-    Route::put('/armadas/{id}/update', [ArmadaController::class, 'update'])->name('armadas.update');
-    Route::delete('/armadas/{id}/destroy', [ArmadaController::class, 'destroy'])->name('armadas.destroy');
-});
+// Armadas
+Route::get('/armadas', [ArmadaController::class, 'index'])->name('armadas.index');
+Route::get('/armadas/create', [ArmadaController::class, 'create'])->name('armadas.create');
+Route::post('/armadas/create', [ArmadaController::class, 'store'])->name('armadas');
+Route::get('/armadas/{id}/edit', [ArmadaController::class, 'edit'])->name('armadas.edit');
+Route::put('/armadas/{id}/update', [ArmadaController::class, 'update'])->name('armadas.update');
+Route::delete('/armadas/{id}/destroy', [ArmadaController::class, 'destroy'])->name('armadas.destroy');
+// });
 
 Auth::routes();
 
